93d6977a9ffe9cb49dd9b1d0cf662bbb09c8b8e6
Paola Tatiana Bernal R <tec_paolab@confamiliares.com>
2024-07-09 16:13:06 -0500

    Ajuste en back pruebas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 const packageJson = require('../../package.json');
 //DEV cta desarrollo https://s6ordoa0ii.execute-api.us-east-1.amazonaws.com/DEV'
-const HOST = 'https://s6ordoa0ii.execute-api.us-east-1.amazonaws.com/DEV';
-//const HOST = 'https://m7co9zv7kk.execute-api.us-east-1.amazonaws.com/DEV';
+//const HOST = 'https://s6ordoa0ii.execute-api.us-east-1.amazonaws.com/DEV';
+const HOST = 'https://m7co9zv7kk.execute-api.us-east-1.amazonaws.com/DEV';
 //const HOST = 'https://3voixiptij.execute-api.us-east-1.amazonaws.com/PD';
 export const environment = {
   API_PUBLIC: HOST + '/',
