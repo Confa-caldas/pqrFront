172a5687b6fcaa49c3c3b82d10db871bf16907cb
Rodrigo Nova T <tec_rodrigon@confamiliares.com>
2024-08-05 14:36:42 -0500

    Eliminacion console.log

--- src/app/components/private/request-details/request-details.component.ts
+++ src/app/components/private/request-details/request-details.component.ts
@@ -189,7 +189,6 @@ export class RequestDetailsComponent implements OnInit {
   }
 
   showProcessTab(): boolean {
-    console.log(this.routeTab);
     if (
       // (this.routeTab.includes(RoutesApp.PROCESS_REQUEST) ||
       //   this.routeTab.includes(RoutesApp.REQUEST_DETAILS)) &&
