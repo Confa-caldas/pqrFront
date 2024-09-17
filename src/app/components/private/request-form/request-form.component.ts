a6b8d43aae0f81ecd463b08263ff484ea1603543
Paola Tatiana Bernal R <tec_paolab@confamiliares.com>
2024-09-16 22:43:20 -0500

    rutas prod

--- src/app/components/private/request-form/request-form.component.ts
+++ src/app/components/private/request-form/request-form.component.ts
@@ -301,7 +301,8 @@ export class RequestFormComponent implements OnInit {
     );
 
     if (this.arrayApplicantAttachment && this.arrayApplicantAttachment.length > 0) {
-      const ruta_archivo_ws = environment.ruta_archivos_ws;
+      const ruta_archivo_ws =
+        'https://alojamiento.confa.co/guardarArchivosRest/guardarArchivo/metodo1';
 
       const estructura = {
         idSolicitud: `${request_id}`,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,19 @@
 const packageJson = require('../../package.json');
 //DEV cta desarrollo https://s6ordoa0ii.execute-api.us-east-1.amazonaws.com/DEV'
 //const HOST = 'https://s6ordoa0ii.execute-api.us-east-1.amazonaws.com/DEV';
-const HOST = 'https://m7co9zv7kk.execute-api.us-east-1.amazonaws.com/DEV';
-//const HOST = 'https://3voixiptij.execute-api.us-east-1.amazonaws.com/PD';
+//const HOST = 'https://m7co9zv7kk.execute-api.us-east-1.amazonaws.com/DEV';
+const HOST = 'https://3voixiptij.execute-api.us-east-1.amazonaws.com/PD';
 //PRUEBAS
 //const RUTAS_ARCHIVOS = 'https://app.confa.co:8687/guardarArchivosRest/guardarArchivo/metodo1';
 //PRODUCCION
-const RUTAS_ARCHIVOS = 'https://alojamiento.confa.co/guardarArchivosRest/guardarArchivo/metodo1';
+//const RUTAS_ARCHIVOS = 'https://alojamiento.confa.co/guardarArchivosRest/guardarArchivo/metodo1';
 export const environment = {
   API_PUBLIC: HOST + '/',
+  // production: false,
+  // context: 'develop',
   production: true,
   context: 'production',
   version: packageJson.version,
   minutesInactive: 15,
-  ruta_archivos_ws: RUTAS_ARCHIVOS,
+  // ruta_archivos_ws: RUTAS_ARCHIVOS,
 };
