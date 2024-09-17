fc93e0dffd52debbc4082bd1446b4984a9ab3e85
Confa-caldas <155458923+Confa-caldas@users.noreply.github.com>
2024-09-17 01:15:09 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@ const packageJson = require('../../package.json');
 const HOST = 'https://m7co9zv7kk.execute-api.us-east-1.amazonaws.com/DEV';
 //const HOST = 'https://3voixiptij.execute-api.us-east-1.amazonaws.com/PD';
 //PRUEBAS
-//const RUTAS_ARCHIVOS = 'https://app.confa.co:8687/guardarArchivosRest/guardarArchivo/metodo1';
+const RUTAS_ARCHIVOS = 'https://app.confa.co:8687/guardarArchivosRest/guardarArchivo/metodo1';
 //PRODUCCION
-const RUTAS_ARCHIVOS = 'https://alojamiento.confa.co/guardarArchivosRest/guardarArchivo/metodo1';
+//const RUTAS_ARCHIVOS = 'https://alojamiento.confa.co/guardarArchivosRest/guardarArchivo/metodo1';
 export const environment = {
   API_PUBLIC: HOST + '/',
   production: false,
