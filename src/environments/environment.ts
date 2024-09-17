9e49f61473da40b712e3ebd901ff3a6adaf57193
Confa-caldas <155458923+Confa-caldas@users.noreply.github.com>
2024-09-16 21:56:50 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 //DEV cta desarrollo https://s6ordoa0ii.execute-api.us-east-1.amazonaws.com/DEV'
 //const HOST = 'https://s6ordoa0ii.execute-api.us-east-1.amazonaws.com/DEV';
-const HOST = 'https://m7co9zv7kk.execute-api.us-east-1.amazonaws.com/DEV';
-//const HOST = 'https://3voixiptij.execute-api.us-east-1.amazonaws.com/PD';
+//const HOST = 'https://m7co9zv7kk.execute-api.us-east-1.amazonaws.com/DEV';
+const HOST = 'https://3voixiptij.execute-api.us-east-1.amazonaws.com/PD';
 //PRUEBAS
 //const RUTAS_ARCHIVOS = 'https://app.confa.co:8687/guardarArchivosRest/guardarArchivo/metodo1';
 //PRODUCCION
@@ -15,5 +15,5 @@ export const environment = {
   context: 'production',
   version: packageJson.version,
   minutesInactive: 15,
-  ruta_archivos_ws: RUTAS_ARCHIVOS,
+  ruta_archivos_ws: RUTAS_ARCHIVOS
 };
