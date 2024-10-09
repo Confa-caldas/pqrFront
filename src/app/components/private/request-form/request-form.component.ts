3b51252850ed0ad561fdd606773cbc0400424f02
Paola Tatiana Bernal R <tec_paolab@confamiliares.com>
2024-10-09 10:16:14 -0500

    ajustes generacion de reporte afiliacion

--- src/app/components/private/request-form/request-form.component.ts
+++ src/app/components/private/request-form/request-form.component.ts
@@ -481,9 +481,8 @@ export class RequestFormComponent implements OnInit {
     this.hasPendingChanges = true;
 
     try {
-    if (this.arrayApplicantAttachment && this.arrayApplicantAttachment.length > 0) {
-      const ruta_archivo_ws =
-        'https://alojamiento.confa.co/guardarArchivosRest/guardarArchivo/metodo1';
+      if (this.arrayApplicantAttachment && this.arrayApplicantAttachment.length > 0) {
+        const ruta_archivo_ws = environment.ruta_archivos_ws;
 
         const estructura = {
           idSolicitud: `${request_id}`,
